15e7a0f871e68e63d48ffbe08c2718fd476769fc
ALDI-JAYA <91648104+ALDI-JAYA@users.noreply.github.com>
2022-09-09 16:20:57 +0700

    new

--- js/main.js
+++ js/main.js
@@ -57,12 +57,12 @@
     });
     
     
-    // Typed Initiate
+    // Tulisan Bergerak
     if ($('.hero .hero-text h2').length == 1) {
         var typed_strings = $('.hero .hero-text .typed-text').text();
         var typed = new Typed('.hero .hero-text h2', {
             strings: typed_strings.split(', '),
-            typeSpeed: 100,
+            typeSpeed: 200,
             backSpeed: 20,
             smartBackspace: false,
             loop: true
